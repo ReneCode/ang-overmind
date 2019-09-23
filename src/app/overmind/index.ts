4b9eeb62ac8b2ad5490ac1843528da406b56757e
Reinhard Langner <Langner.R@eplan.de>
2019-09-23 08:29:55 +0200

    remove di try

--- src/app/overmind/index.ts
+++ src/app/overmind/index.ts
@@ -4,7 +4,10 @@ import { OvermindService } from "overmind-angular";
 import { state } from "./state";
 import * as actions from "./actions";
 
-export const config = { state, actions, di: { httpClient: null } };
+export const config = { state, actions };
+
+
+// this is necessary to startup with overmind
 
 // @ts-ignore
 if (!window.process) {
